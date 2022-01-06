29495875440b7160f23c2cd0961aeae7b6219cd9
Akshit Kumar <88525453+AkshitKumar13@users.noreply.github.com>
2022-01-06 14:28:22 +0530

    Update RandomNumber.java

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -4,15 +4,24 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
- 
+
+/**
+ * The type Random number.
+ */
 public class RandomNumber {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
     public static void main(String...args) {
          List<Integer> numbersList = new ArrayList<>();
-
         Random random = new Random();
         random.ints(100,1,100).forEach(numbersList::add);
-List<Integer> streamList=numbersList.stream().filter(number->IntStream.rangeClosed(2,number/2).noneMatch(i -> number%i==0)).collect(Collectors.toList());
-        System.out.println(streamList);
+        System.out.println("Random List "+numbersList);
+
+        List<Integer> streamList=numbersList.stream().filter(number->IntStream.rangeClosed(2,number/2).noneMatch(i -> number%i==0)).collect(Collectors.toList());
+        System.out.println("Prime Numbers "+streamList);
 
     }
 
